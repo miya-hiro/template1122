1054098defba7abd0faad027729b05672a7e39e8
miya-hiro <hiro.noon3@gmail.com>
2020-12-11 10:01:11 +0900

    js全体修正

--- js/script.js
+++ js/script.js
@@ -1,42 +1,35 @@
 // ローディング判定
-jQuery(window).on("load", function() {
-	jQuery("body").attr("data-loading", "true");
+jQuery(window).on("load", function () {
+
+	navColor();
+
 });
 
-jQuery(function() {
-	// スクロール判定
-	jQuery(window).on("scroll", function() {
-		let scrollTop = jQuery(this).scrollTop();
-		let windowHeight = jQuery(this).height();
-		let documentHeight = jQuery(document).height();
-
-		if (100 < scrollTop) {
-			jQuery("body").attr("data-scroll", "true");
-		} else {
-			jQuery("body").attr("data-scroll", "false");
-		}
-
-		if (documentHeight - (windowHeight + scrollTop) <= 0) {
-			jQuery("body").attr("data-scroll-bottom", "true");
-		} else {
-			jQuery("body").attr("data-scroll-bottom", "false");
-		}
-	});
+jQuery(function () {
 	
+// mv以下にスクロールしたら
+	jQuery(window).on("scroll", function () {
+		navColor();
+	});
+
 	/* ドロワー */
-	jQuery(".js-drawer").on("click", function() {
+	jQuery(".js-drawer").on("click", function () {
 		var targetClass = jQuery(this).attr("data-target");
 		jQuery("." + targetClass).toggleClass("is-checked");
-
 	});
 
 	/* スムーススクロール */
-	jQuery('a[href^="#"]').click(function() {
-		let header = jQuery("#header").height();
-		let speed = 300;
-		let id = jQuery(this).attr("href");
-		let target = jQuery("#" == id ? "html" : id);
-		let position = jQuery(target).offset().top - header;
+	jQuery('a[href^="#"]').click(function () {
+		var header = jQuery("#header").height();
+		var speed = 300;
+		var id = jQuery(this).attr("href");
+		var target = jQuery("#" == id ? "html" : id);
+		var position = jQuery(target).offset().top - header;
+
+		$('.drawer').find('.is-checked').each(function () {
+			$(this).removeClass('is-checked');
+		});
+
 		if ("fixed" !== jQuery("#header").css("position")) {
 			position = jQuery(target).offset().top;
 		}
@@ -57,8 +50,35 @@ jQuery(function() {
 	if (ua.indexOf("iPhone") < 0 && ua.indexOf("Android") < 0) {
 		jQuery('a[href^="tel:"]')
 			.css("cursor", "default")
-			.on("click", function(e) {
+			.on("click", function (e) {
 				e.preventDefault();
 			});
 	}
+
+	var mySwiper = new Swiper('.swiper-container', {
+		loop: true, // 最後のスライドまで到達した場合、最初に戻らずに続けてスライド可能にするか。
+		speed: 600, // スライドが切り替わるトランジション時間(ミリ秒)。
+		slidesPerView: 1, // 何枚のスライドを表示するか
+		// spaceBetween: 10, // スライド間の余白サイズ(ピクセル)
+		// direction: 'horizontal', // スライド方向。 'horizontal'(水平) か 'vertical'(垂直)。effectオプションが 'slide' 以外は無効。
+		effect: 'fade', // "slide", "fade"(フェード), "cube"(キューブ回転), "coverflow"(カバーフロー) または "flip"(平面回転)
+ 
+		// スライダーの自動再生
+		// autoplay: true 　のみなら既定値での自動再生
+		autoplay: {
+			delay: 3000, // スライドが切り替わるまでの表示時間(ミリ秒)
+			stopOnLast: false, // 最後のスライドまで表示されたら自動再生を中止するか
+			disableOnInteraction: true // ユーザーのスワイプ操作を検出したら自動再生を中止するか
+		},
+	});
+
 });
+
+/* 関数定義 */
+function navColor() {
+	var scrollTop = $('.header').outerHeight() - 50;
+	var $header = $('.header');
+	var $body = $('body');
+
+	$(window).scrollTop() > scrollTop ? $body.addClass("scrolled") : $body.removeClass("scrolled");
+}
